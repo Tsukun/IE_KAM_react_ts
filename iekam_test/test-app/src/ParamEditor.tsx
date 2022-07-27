4f2a6d5f3c82e98d51cdf30f46f7aeef399e6fd5
Tsukune <tsukuneaune@gmail.com>
2022-07-28 00:19:10 +0700

    add values in array

--- iekam_test/test-app/src/ParamEditor.tsx
+++ iekam_test/test-app/src/ParamEditor.tsx
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
 
 interface IParamValue {
@@ -27,6 +28,9 @@ interface IState {
   IParam: IParam[];
   IModel: IModel;
   toggle: boolean;
+  name: string,
+  type: string,
+  value: string,
 }
 
 export default class ParamEditor extends React.Component<IProps, IState> {
@@ -37,6 +41,9 @@ export default class ParamEditor extends React.Component<IProps, IState> {
       IModel: props.model,
       IParam: props.params,
       toggle: true,
+      name: "",
+      type:"",
+      value:"",
     };
   }
   render() {
@@ -49,8 +56,8 @@ export default class ParamEditor extends React.Component<IProps, IState> {
                 if (dataM.paramId === dataP.id) {
                   return (
                     <div>
-                      <h2>{dataP.name}</h2>
-                      <input
+                      <h2>Название: {dataP.name}</h2>
+                      Значение: <input
                         id={String(dataP.id)}
                         onChange={e => this.changeValue(Number(e.target.id), e.target.value)}
                         defaultValue={dataM.value}
@@ -70,20 +77,20 @@ export default class ParamEditor extends React.Component<IProps, IState> {
           {!this.state.toggle && (
             <div>
               <h1>Введите параметры</h1>
-              <form onSubmit={e => this.setNewParam(e)}>
+              <form onSubmit={this.handleSubmit}>
                 <div>
                   <label htmlFor='name'>Название</label>
-                  <input id='name' />
+                  <input id='name' onChange={(e) => this.setState({name: e.target.value})} />
                 </div>
                 <div>
                   <label htmlFor='type'>Тип</label>
-                  <input id='type' />
+                  <input id='type' onChange={(e) => this.setState({type: e.target.value})}/>
                 </div>
                 <div>
                   <label htmlFor='value'>Значение</label>
-                  <input id='value' />
+                  <input id='value' onChange={(e) => this.setState({value: e.target.value})}/>
                 </div>
-                <button>Добавить</button>
+                <input type="submit" value="Добавить"/>
               </form>
             </div>
           )}
@@ -93,13 +100,33 @@ export default class ParamEditor extends React.Component<IProps, IState> {
   }
 
   changeValue = (id: number, value: string) => {
-    this.state.IModel.paramValues.map((element, index) => {
+    let items: IModel = this.state.IModel
+    items.paramValues.map((element, index) => {
       if (element.paramId === id) {
-        this.state.IModel.paramValues[index].value = value;
+        items.paramValues[index].value = value;
+        this.setState({IModel: items})
       }
     });
     console.log(this.state.IModel.paramValues);
   };
 
-  setNewParam = (e: React.FormEvent) => {};
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+    let max = 0
+    console.log(this.state.IModel)
+    let arrayModel:IModel = this.state.IModel
+    let arrayParams: IParam[] = this.state.IParam
+    arrayModel.paramValues.map(value=>{
+      if(value.paramId > max){
+        max = value.paramId
+      }
+    })
+    arrayModel.paramValues.push({"paramId": max +1, "value": this.state.value})
+    arrayParams.push({"id": max+1, "name": this.state.name, "type": this.state.type})
+    this.setState({IModel: arrayModel})
+    this.setState({IParam: arrayParams})
+    
+    console.log(this.state.IModel)
+    console.log(this.state.IParam)
+  }
 }
